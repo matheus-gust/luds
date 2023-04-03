536dbcb8b63cb78f2653649b23af8658677f9e79
matheus-gust <matheus_guga2012@hotmail.com>
2023-04-02 23:59:40 -0300

    Adicionando solicitação de insumo

--- src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
+++ src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
@@ -5,21 +5,22 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
-    @Configuration
+@Configuration
     public class MvcConfiguration implements WebMvcConfigurer {
 
         @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            String resource = getClass().getClassLoader().getResource("webapp/luds-front").getPath();
-            String pathAlterado = resource.substring(resource.length() - 1).equals(File.separator) ? resource
-                    : resource + File.separator;
+            String prop = fetchFront().getPath();
+            String pathAlterado = prop.substring(prop.length() - 1).equals(File.separator) ? prop
+                    : prop + File.separator;
             System.out.println(pathAlterado);
             registry.addResourceHandler("/front/**", "/front", "/front/")
                     .addResourceLocations("file:" + pathAlterado)
@@ -33,5 +34,16 @@ import java.io.IOException;
                                     : new FileUrlResource(pathAlterado + "index.html");
                         }
                     });
+        }
+
+        public static File fetchFront(){
+            File file = null;
+            try {
+                file = ResourceUtils.getFile("classpath:webapp/luds-front");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            return file;
         }
     }
