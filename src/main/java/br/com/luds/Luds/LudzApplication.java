39c0b068bd1e1f874814878f0e2c9f74f913a7f5
matheus-gust <matheus_guga2012@hotmail.com>
2023-04-03 01:10:30 -0300

    Adicionando solicitação de insumo

--- src/main/java/br/com/luds/Luds/LudzApplication.java
+++ src/main/java/br/com/luds/Luds/LudzApplication.java
@@ -2,8 +2,10 @@ package br.com.luds.Luds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class LudzApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
+++ src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -16,41 +17,26 @@ import java.io.*;
 import java.util.Properties;
 
 @Configuration
-    public class MvcConfiguration implements WebMvcConfigurer {
+public class MvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private ResourceLoader resourceLoader;
 
-        @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            String prop = fetchFront().getPath();
-            String pathAlterado = prop.substring(prop.length() - 1).equals(File.separator) ? prop
-                    : prop + File.separator;
-            System.out.println(pathAlterado);
-            registry.addResourceHandler("/front/**", "/front", "/front/")
-                    .addResourceLocations("file:" + pathAlterado)
-                    .resourceChain(true)
-                    .addResolver(new PathResourceResolver() {
-                        @Override
-                        protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                            Resource requestedResource = location.createRelative(resourcePath);
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String webapps = "/templates/luds-front/";
+        registry.addResourceHandler("/front/**", "/front", "/front/")
+                .addResourceLocations("classpath:" + webapps)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
 
-                            return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                    : new FileUrlResource(pathAlterado + "index.html");
-                        }
-                    });
-        }
-
-        public File fetchFront(){
-            File file = null;
-            try {
-                file = resourceLoader.getResource("classpath:webapp/luds-front").getFile();
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return file;
-        }
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource(webapps + "index.html");
+                    }
+                });
     }
+
+}
