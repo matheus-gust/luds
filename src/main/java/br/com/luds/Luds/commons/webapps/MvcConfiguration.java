d616fb7142a083233c952ee64eb38472218bf968
matheus-gust <matheus_guga2012@hotmail.com>
2023-04-02 23:44:00 -0300

    adding front end

--- src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
+++ src/main/java/br/com/luds/Luds/commons/webapps/MvcConfiguration.java
@@ -13,16 +13,14 @@ import java.io.File;
 import java.io.IOException;
 
     @Configuration
-    @ConditionalOnProperty("mpb.frontend.path")
     public class MvcConfiguration implements WebMvcConfigurer {
-
-        @Value("${front.path}")
-        private String frontEndPath;
 
         @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            String pathAlterado = frontEndPath.substring(frontEndPath.length() - 1).equals(File.separator) ? frontEndPath
-                    : frontEndPath + File.separator;
+            String resource = getClass().getClassLoader().getResource("webapp/luds-front").getPath().substring(1);
+            String pathAlterado = resource.substring(resource.length() - 1).equals(File.separator) ? resource
+                    : resource + File.separator;
+            System.out.println(pathAlterado);
             registry.addResourceHandler("/front/**", "/front", "/front/")
                     .addResourceLocations("file:" + pathAlterado)
                     .resourceChain(true)
